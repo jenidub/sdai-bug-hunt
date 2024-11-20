0081c1f9f03b3cdff5ebed682c5bab13fac25764
Jeni Williams <stanfordjeni@gmail.com>
2024-11-20 04:44:58 +0000

    Found all the bugs - 3 in HTML, 3 in JS

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-getElementById('funfactButton').addEventListener('click', function() {
-  const funFact = document.getElementById('funfact');
+// JS Bug #1 Line 1 - requires document.getElementById('')
+// JS Bug #2 Line 1 - the ID funfactButton should have been funFactButton
+document.getElementById('funFactButton').addEventListener('click', function() {
+  // JS Bug #3 Line 4 - the ID funfact should have been funFact  
+  const funFact = document.getElementById('funFact');
   if (funFact.style.display === 'none') {
       funFact.style.display = 'block';
   } else {
